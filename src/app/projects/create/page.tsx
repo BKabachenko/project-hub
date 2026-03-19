edf58442c56445e8b1c3ced803c98192ed55056b
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-03-19 14:38:46 +0200

    feat(CreateProjectPage): create new page

---
+++ src/app/projects/create/page.tsx
@@ -0,0 +1,12 @@
+import CreateProjectForm from '@/features/projects/CreateProjectForm';
+
+const CreateProjectPage = () => {
+  return (
+    <div>
+      {'Create project form'}
+      <CreateProjectForm />
+    </div>
+  );
+};
+
+export default CreateProjectPage;
