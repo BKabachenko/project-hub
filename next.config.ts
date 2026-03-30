7ed4c247c4c51406fc9a80e8e5c1ea0a37ec1cbe
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-03-30 19:08:23 +0300

    feat(next config): add remote img host

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 
-  serverExternalPackages: ["@prisma/client", "pg", "prisma"],
+  serverExternalPackages: ['@prisma/client', 'pg', 'prisma'],
 
   turbopack: {
     resolveAlias: {
-      "@prisma/client": "./src/generated/prisma",
+      '@prisma/client': './src/generated/prisma',
     },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
+    ],
   },
 
   allowedDevOrigins: process.env.MY_LOCAL_DEV_IP ? [process.env.MY_LOCAL_DEV_IP] : undefined,
