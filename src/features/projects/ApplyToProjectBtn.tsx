a47f0a88bcbc07d774e5d0b891170ff7b3f5a2ac
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-03 14:01:57 +0300

    feat(apply to project form): replace select element with custom UI components

--- src/features/projects/ApplyToProjectBtn.tsx
+++ src/features/projects/ApplyToProjectBtn.tsx
@@ -1,37 +1,57 @@
-'use client'
+'use client';
 import { useActionState } from 'react';
 
+import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { applyToProjectAction } from '@/features/projects/applyToProjectAction';
 import { initialState } from '@/lib/constants';
-
 
 const ApplyToProjectForm = ({ projectId }: { projectId: string }) => {
   const [state, applyAction, isPending] = useActionState(applyToProjectAction, initialState);
 
   return (
     <form action={applyAction}>
-      <input type='text' name='projectId' id='projectId' hidden={true} value={projectId} readOnly={true}/>
-      <select name='memberRole' id='memberRole' defaultValue={''}>
-        <option value="" disabled>Choose role in project</option>
-        <option value='FRONTEND_DEVELOPER'>FRONTEND_DEVELOPER</option>
-        <option value='BACKEND_DEVELOPER'>BACKEND_DEVELOPER</option>
-        <option value='FULLSTACK_DEVELOPER'>FULLSTACK_DEVELOPER</option>
-        <option value='PROJECT_MANAGER'>PROJECT_MANAGER</option>
-        <option value='UI_UX_DESIGNER'>UI_UX_DESIGNER</option>
-        <option value='QA_ENGINEER'>QA_ENGINEER</option>
-        <option value='DEVOPS_ENGINEER'>DEVOPS_ENGINEER</option>
-        <option value='BUSINESS_ANALYST'>BUSINESS_ANALYST</option>
-        <option value='DATA_SCIENTIST'>DATA_SCIENTIST</option>
-        <option value='MARKETING_SPECIALIST'>MARKETING_SPECIALIST</option>
-        <option value='WRONG'>WRONG</option>
-      </select>
+      <input
+        type='text'
+        name='projectId'
+        id='projectId'
+        hidden={true}
+        value={projectId}
+        readOnly={true}
+      />
+      <Select name='memberRole' defaultValue={''}>
+        <SelectTrigger className='w-100'>
+          <SelectValue placeholder='Choose role in project' />
+        </SelectTrigger>
+        <SelectContent position={'popper'}>
+          <SelectGroup>
+            <SelectItem value='FRONTEND_DEVELOPER'>FRONTEND_DEVELOPER</SelectItem>
+            <SelectItem value='BACKEND_DEVELOPER'>BACKEND_DEVELOPER</SelectItem>
+            <SelectItem value='FULLSTACK_DEVELOPER'>FULLSTACK_DEVELOPER</SelectItem>
+            <SelectItem value='PROJECT_MANAGER'>PROJECT_MANAGER</SelectItem>
+            <SelectItem value='UI_UX_DESIGNER'>UI_UX_DESIGNER</SelectItem>
+            <SelectItem value='QA_ENGINEER'>QA_ENGINEER</SelectItem>
+            <SelectItem value='DEVOPS_ENGINEER'>DEVOPS_ENGINEER</SelectItem>
+            <SelectItem value='BUSINESS_ANALYST'>BUSINESS_ANALYST</SelectItem>
+            <SelectItem value='DATA_SCIENTIST'>DATA_SCIENTIST</SelectItem>
+            <SelectItem value='MARKETING_SPECIALIST'>MARKETING_SPECIALIST</SelectItem>
+            <SelectItem value='WRONG'>WRONG</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       {state.fieldErrors?.memberRole && <p>{state.fieldErrors.memberRole}</p>}
-      <button className='cursor-pointer' type='submit' disabled={isPending}>
+      <Button variant={'default'} type='submit' disabled={isPending}>
         {isPending ? 'Wait...' : 'Apply'}
-      </button>
+      </Button>
       {state.message && <p>{state.message}</p>}
     </form>
-
   );
 };
 
