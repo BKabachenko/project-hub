28a4fe12008999d389094d981accc51a8f3bc466
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-04 15:58:01 +0300

    feat(header): add navigation and login\logout functionality

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import Link from 'next/link';
 
-const Header = () => {
+import { auth, signIn, signOut } from '@/auth';
+
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Button } from '../ui/button';
+
+const Header = async () => {
+  const session = await auth();
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className='mb-10'>
+      <div className='bg-background border-border mx-auto grid max-w-7xl grid-cols-[auto_1fr_auto] items-center rounded-lg border px-8 py-5'>
+        <Link href={'/'}>
+          <div className=''>Project Hub</div>
+        </Link>
+        <nav className='flex justify-end gap-x-5 px-10'>
+          <Link href={'/'} className=''>
+            Home
+          </Link>
+          <Link href={'/feed'} className=''>
+            Feed
+          </Link>
+          <Link href={'/impact'} className=''>
+            Impact
+          </Link>
+          {session ? (
+            <Link href={'/dashboard'} className=''>
+              Dashboard
+            </Link>
+          ) : (
+            ''
+          )}
+        </nav>
+        <div className=''>
+          {session ? (
+            <form
+              action={async () => {
+                'use server';
+                await signOut();
+              }}
+            >
+              <button type='submit' className={'translate-y-0.5'}>
+                <Avatar>
+                  <AvatarImage
+                    src={session.user?.image ?? undefined}
+                    alt={'User avatar'}
+                    title={'Sign Out'}
+                  />
+                  <AvatarFallback>{session.user?.name?.[0]?.toUpperCase() || 'U'}</AvatarFallback>
+                </Avatar>
+              </button>
+            </form>
+          ) : (
+            <form
+              action={async () => {
+                'use server';
+                await signIn();
+              }}
+            >
+              <Button variant={'default'} type='submit'>
+                Sign In
+              </Button>
+            </form>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
