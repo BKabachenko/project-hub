dbbae38881aae2316753e81f951bea17db9f6ca5
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-13 15:28:25 +0300

    feat(badge ui): add one new variant

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -19,6 +19,9 @@ const badgeVariants = cva(
         ghost:
           "hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50",
         link: "text-primary underline-offset-4 hover:underline",
+        role: 
+          "bg-background p-3 rounded-md border-border/80 hover:bg-muted dark:hover:bg-muted/50 font-normal",
+          
       },
     },
     defaultVariants: {
