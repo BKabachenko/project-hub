859fb9cd2540ab2e9fcb5f6f580bf08dd19fa351
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-16 12:30:43 +0300

    feat(ProjectCard): implement mobile view

--- src/app/(main)/projects/_components/ProjectCard.tsx
+++ src/app/(main)/projects/_components/ProjectCard.tsx
@@ -18,25 +18,25 @@ import ProjectTypeBadge from './ProjectTypeBadge';
 
 const ProjectCard = ({ project }: { project: ProjectWithPositions }) => {
   return (
-    <Card className={'max-w-175 shadow-md p-2 py-6'}>
-      <CardHeader>
+    <Card className={'max-w-175 p-2 py-6 shadow-md'}>
+      <CardHeader className={'flex flex-col md:grid'}>
         <Link href={`/projects/${project.id}`}>
           <CardTitle className={'text-2xl font-bold'}>{project.title}</CardTitle>
         </Link>
         <CardDescription>
-          <div className='text-sm'>Posted 2 hours ago</div>
+          {/*TODO: add real create time*/}
+          <div className='text-muted-foreground text-sm'>Posted 2 hours ago</div>{' '}
         </CardDescription>
-        <CardAction className={'flex gap-2'}>
+        <CardAction className={'order-first mb-4 flex gap-2 md:order-last'}>
           {project.type.map((type) => (
             <ProjectTypeBadge key={type} type={type} />
           ))}
           <ProjectStatusBadge status={project.status} />
-          {}
         </CardAction>
       </CardHeader>
       <CardContent className={'flex flex-col gap-2'}>
         <p className={'line-clamp-3'}>{project.description}</p>
-        <div className='flex flex-row flex-wrap items-center justify-between'>
+        <div className='flex flex-row flex-wrap items-center justify-between gap-4'>
           <div className={'flex flex-row flex-wrap gap-2'}>
             {project.projectPositions.map((position) => (
               <Badge key={position.role} variant={'role'}>
@@ -44,8 +44,11 @@ const ProjectCard = ({ project }: { project: ProjectWithPositions }) => {
               </Badge>
             ))}
           </div>
-          <Link href={`/projects/${project.id}`} className={'font-semibold hover:underline'}>
-            {'Details...'}
+          <Link
+            href={`/projects/${project.id}`}
+            className={'ml-auto font-semibold underline md:no-underline md:hover:underline'}
+          >
+            Details...
           </Link>
         </div>
       </CardContent>
