5c4695d4d584e63be8e8fce13019d5def8d263cc
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-24 00:00:54 +0300

    feat(utils): add new time utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function timeAgo(dateString: Date) {
+  const date: Date = new Date(dateString);
+  const now: Date = new Date();
+  const diffInSeconds = Math.round((date.getTime() - now.getTime()) / 1000); 
+
+  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+
+  const intervals: { limit: number; unit: Intl.RelativeTimeFormatUnit }[] = [
+    { limit: 60, unit: 'second' },
+    { limit: 3600, unit: 'minute' },
+    { limit: 86400, unit: 'hour' },
+    { limit: 2592000, unit: 'day' },
+    { limit: 31536000, unit: 'month' },
+    { limit: Infinity, unit: 'year' }
+  ];
+
+  for (const interval of intervals) {
+    if (Math.abs(diffInSeconds) < interval.limit) {
+      const val = Math.round(diffInSeconds / (interval.limit === 60 ? 1 : intervals[intervals.indexOf(interval) - 1].limit));
+      return rtf.format(val, interval.unit);
+    }
+  }
+}
