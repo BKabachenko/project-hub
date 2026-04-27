e125044fdd6c08a784c239a1a6da106b571454a9
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-04-27 19:00:27 +0300

    feat(Dashboard page): add logic and refactor StatisticCardsBlock

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { ChevronDown, FileUser, Users } from 'lucide-react';
-
 import { auth } from '@/auth';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import prisma from '@/lib/prisma';
 
 import MemberProjectsBlock from './_components/MemberProjectsBlock';
 import OwnerProjectBlock from './_components/OwnerProjectBlock';
-import OwnerProjectCard from './_components/OwnerProjectCard';
+import StatisticCardsBlock from './_components/StatisticCardsBlock';
 
 const DashboardPage = async () => {
   const session = await auth();
@@ -53,57 +50,13 @@ const DashboardPage = async () => {
 
   const userName = session.user?.name?.split(' ')[0];
 
-  const totalOpenProjects = projects.length;
-
-  const totalOwnACtiveProjects = projects.reduce((sum, project) => {
-    if (project.authorId === session.user?.id) return sum + 1;
-    return sum;
-  }, 0);
-  const countPendingReviews = projects.reduce((sum, project) => {
-    return sum + project.projectMembers.length;
-  }, 0);
-
   return (
     <div className={'flex flex-col gap-8 md:gap-12'}>
       <div className='flex flex-col gap-2'>
         <p className={'text-muted-foreground font-medium'}>DASHBOARD OVERVIEW</p>
         <h2 className={'text-4xl font-extrabold'}>Welcome back, {userName}!</h2>
       </div>
-      <div className='grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4'>
-        <Card className={'relative shadow-md'}>
-          <CardHeader>
-            <CardTitle className={'text-muted-foreground'}>TOTAL PROJECTS</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className='text-3xl font-extrabold'>{totalOpenProjects}</div>
-          </CardContent>
-          <div className='absolute top-7 right-5 flex h-12 w-12 items-center justify-center rounded-lg bg-purple-200'>
-            <Users />
-          </div>
-        </Card>
-        <Card className={'relative shadow-md'}>
-          <CardHeader>
-            <CardTitle className={'text-muted-foreground'}>YOUR ACTIVE PROJECTS</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className='text-3xl font-extrabold'>{totalOwnACtiveProjects}</div>
-          </CardContent>
-          <div className='absolute top-7 right-5 flex h-12 w-12 items-center justify-center rounded-lg bg-purple-200'>
-            <Users />
-          </div>
-        </Card>
-        <Card className={'relative shadow-md'}>
-          <CardHeader>
-            <CardTitle className={'text-muted-foreground'}>PENDING APPLICATIONS</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className='text-3xl font-extrabold'>{countPendingReviews}</div>
-          </CardContent>
-          <div className='absolute top-7 right-5 flex h-12 w-12 items-center justify-center rounded-lg bg-purple-200'>
-            <Users />
-          </div>
-        </Card>
-      </div>
+      <StatisticCardsBlock projects={projects} />
       <OwnerProjectBlock projects={userOwnedProjects} />
       <MemberProjectsBlock projects={userParticipantProjects} />
     </div>
