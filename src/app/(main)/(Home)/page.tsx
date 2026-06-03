1a20e6b35a44eddee29760604192f30563d28d47
Bogdan-homepc <bogdankabachenko2015@gmail.com>
2026-06-03 23:41:58 +0300

    feat(SearchBar): refactor

--- src/app/(main)/(Home)/page.tsx
+++ src/app/(main)/(Home)/page.tsx
@@ -1,12 +1,10 @@
-import { SearchIcon } from 'lucide-react';
-
 import ProjectCard from '@/app/(main)/projects/_components/ProjectCard';
-import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
 import prisma from '@/lib/prisma';
 
 import FilterBadge from './_components/FilterBadge';
 import FilterBlock from './_components/FilterBlock';
 import FilterSheet from './_components/FilterSheet';
+import SearchBar from '@/features/feed/SearchBar';
 
 export default async function Home() {
   const newTenProjects = await prisma.project.findMany({
@@ -24,17 +22,7 @@ export default async function Home() {
       <div className='flex flex-col justify-center gap-4 md:flex-row'>
         <div className='flex flex-col gap-8'>
           <div className='flex flex-row justify-between gap-2'>
-            <InputGroup className={'bg-input border-border p-6 shadow-md'}>
-              <InputGroupInput
-                id='inline-start-input'
-                placeholder='Search...'
-                type={'search'}
-                className={'rounded-md'}
-              />
-              <InputGroupAddon align='inline-start'>
-                <SearchIcon className='text-muted-foreground' />
-              </InputGroupAddon>
-            </InputGroup>
+            <SearchBar />
             <div className='border-border bg-input w-16 rounded-lg border shadow-md md:hidden'>
               <FilterSheet />
             </div>
---
+++ src/features/feed/SearchBar.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { SearchIcon } from 'lucide-react';
+
+import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
+
+export const debounce = <Args extends unknown[]>(func: (...args: Args) => void, delay: number) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (...args: Args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
+
+const SearchBar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const defaultParam = searchParams.get('query')?.toString() || '';
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
+  const debouncedHandleSearch = debounce(handleSearch, 300);
+
+  return (
+    <InputGroup className={'bg-input border-border p-6 shadow-md'}>
+      <InputGroupInput
+        id='inline-start-input'
+        placeholder='Search...'
+        type={'search'}
+        className={'rounded-md'}
+        defaultValue={defaultParam}
+        onChange={(e) => debouncedHandleSearch(e.currentTarget.value)}
+      />
+      <InputGroupAddon align='inline-start'>
+        <SearchIcon className='text-muted-foreground' />
+      </InputGroupAddon>
+    </InputGroup>
+  );
+};
+
+export default SearchBar;
